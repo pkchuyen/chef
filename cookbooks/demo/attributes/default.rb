2ef2c85b444ffc9f8cfedbb0ffa80e956722c450
Chuyen Pham <pkchuyen@gmail.com>
2017-01-24 01:41:29 -0600

    adjust attributes file to allow root login and update ssh port

--- cookbooks/demo/attributes/default.rb
+++ cookbooks/demo/attributes/default.rb
@@ -4,10 +4,10 @@ default.users = ['deployuser']
 # Don't create an ssh key for us
 default.user.ssh_keygen = false
 
-default.openssh.server.permit_root_login = 'no'
+default.openssh.server.permit_root_login = 'yes'
 default.openssh.server.password_authentication = 'no'
 default.openssh.server.allow_groups = 'sudo'
-default.openssh.server.port = '27984'
+default.openssh.server.port = '22'
 default.openssh.server.login_grace_time = '30'
 default.openssh.server.use_p_a_m = 'no'
 default.openssh.server.print_motd = 'no'
